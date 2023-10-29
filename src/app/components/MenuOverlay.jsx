de9d6584f00f8095fe8a9327a865416967cb17ba
CatUnderNewTent <kviatkovsky21@gmail.com>
2023-10-29 23:42:12 +0400

    uploading portfolio website with created navbar

---
+++ src/app/components/MenuOverlay.jsx
@@ -0,0 +1,15 @@
+import NavLink from './NavLink';
+
+const MenuOverlay = ({ links }) => {
+  return (
+    <ul className='flex flex-col py-4 items-center'>
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.path} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MenuOverlay;
